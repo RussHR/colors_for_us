3b89156c5de635baf5870b01bcb2da7ce619e9b4
RussHR <russ.rinzler@gmail.com>
2013-11-05 15:08:40 -0800

    Use Sendgrid for emails

--- Gemfile
+++ Gemfile
@@ -13,6 +13,7 @@ end
 group :development do
   gem 'better_errors'
   gem 'binding_of_caller'
+  gem 'letter_opener'
 end
 
 gem 'figaro'
--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,19 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the rails application
 ColorsForUs::Application.initialize!
+
+if Rails.env.production?
+  # only send real emails in production using Sendgrid
+  ActionMailer::Base.smtp_settings = {
+    :address        => 'smtp.sendgrid.net',
+    :port           => '587',
+    :authentication => :plain,
+    :user_name      => ENV['SENDGRID_USERNAME'],
+    :password       => ENV['SENDGRID_PASSWORD'],
+    :domain         => 'heroku.com',
+    :enable_starttls_auto => true
+  }
+elsif Rails.env.development?
+  # use the letter_opener gem to open email in new tab
+  ActionMailer::Base.delivery_method = :letter_opener
+end
