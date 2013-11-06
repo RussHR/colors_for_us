2f529116dba623ba6e4ee8ca5c4e1bea500d665b
RussHR <russ.rinzler@gmail.com>
2013-11-06 15:42:59 -0800

    Index renders palette colors

--- app/assets/javascripts/models/palette.js
+++ app/assets/javascripts/models/palette.js
@@ -1,3 +1,24 @@
 ColorsForUs.Models.Palette = Backbone.Model.extend({
-  urlRoot: "/palettes"
+  urlRoot: "/palettes",
+  
+  numOfColors: function() {
+    return this.colors().length
+  },
+  
+  divPercentWidth: function() {
+    return (1 / this.numOfColors()) * 100;
+  },
+  
+  colors: function() {
+    var colorArray = [];
+    
+    for (var i = 1; i < 6; i++) {
+      var possibleColor = this.get("color_" + i);
+      if (typeof possibleColor === "string") {
+        colorArray.push(possibleColor)
+      }
+    }
+    
+    return colorArray;
+  }
 });
