8dbffe0ba7af8678517041c04dbce9b04aa6bd1c
RussHR <russ.rinzler@gmail.com>
2013-11-07 11:21:22 -0800

    Color selection changes div colors and div attr

--- app/assets/javascripts/views/palettes/new_palette.js
+++ app/assets/javascripts/views/palettes/new_palette.js
@@ -1,10 +1,27 @@
 ColorsForUs.Views.NewPalette = Backbone.View.extend({
   template: JST['palettes/new'],
   
+  events: {
+    "click .future-color": "setActiveColor"
+  },
+  
+  setActiveColor: function(event) {
+    var $currentSwatch = $(event.target);
+    $currentSwatch.addClass('active-swatch');
+    $currentSwatch.siblings().removeClass('active-swatch');
+  },
+  
   render: function() {
     var renderedContent = this.template();
     this.$el.html(renderedContent);
-    this.$el.find('#color-picker').ColorPicker({flat: true});
+    this.$el.find('#color-picker').ColorPicker({
+      flat: true,
+      
+      onChange: function(hsb, hex, rgb) {
+        $('.active-swatch').css('background-color', '#' + hex);
+        $('.active-swatch').attr('data-color', '#' + hex);
+      },
+    });
     return this;
   }   
 });
