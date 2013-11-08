e7e58f0081e1318dd79b3d0dc5170dc5e6437452
RussHR <russ.rinzler@gmail.com>
2013-11-08 15:38:11 -0800

    Generate Wallpapers controller

---
+++ app/controllers/wallpapers_controller.rb
@@ -0,0 +1,5 @@
+class WallpapersController < ApplicationController
+  def show
+    render :show
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,9 @@ ColorsForUs::Application.routes.draw do
   devise_for :users, :controllers => {:registrations => 'registrations'}
   resources :authentications, :only => [:index, :create, :destroy]
   
-  resources :palettes, :only => [:index, :create, :show]
+  resources :palettes, :only => [:index, :create, :show] do
+    resource :wallpaper, :only => [:show]
+  end
   resources :favorites, :only => [:create, :destroy]
   root to: "Root#root"
 end
