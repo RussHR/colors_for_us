4bd66b4c2276da3c182e820c5dd2dc827e8b4df3
RussHR <russ.rinzler@gmail.com>
2013-11-08 14:37:04 -0800

    Favorite/unfavorite buttons work on palette detail page

--- app/assets/javascripts/views/palettes/palette_detail.js
+++ app/assets/javascripts/views/palettes/palette_detail.js
@@ -1,6 +1,61 @@
 ColorsForUs.Views.PaletteDetail = Backbone.View.extend({
   template: JST['palettes/detail'],
   
+  events: {
+    "click button": "toggleFavorite"
+  },
+  
+  toggleFavorite: function(event) {
+    var $button = $(event.currentTarget);
+    var $buttons = $button.parent();
+
+    if ($button.attr('class') === "favorite-button") {
+      this.createFavorite();
+    } 
+    else {
+      this.removeFavorite();
+    }
+    
+    $buttons.toggleClass('favorite unfavorite');
+  },
+  
+  createFavorite: function() {
+    var favorite = new ColorsForUs.Models.Favorite({
+      palette_id: this.model.get('id')
+    });
+    
+    favorite.save({
+      success: function(model) {
+        console.log("you favorited this!");
+      }
+    });
+  },
+  
+  removeFavorite: function() {
+    var that = this;
+    
+    this.model.fetch({
+      success: function() {
+        that.model.get('favorites').forEach(function(favorite) {
+          if (favorite.giver_id === that.currentUserID &&
+              favorite.palette_id === parseInt(that.model.get('id'))) {
+                that.deleteFavoriteByID(favorite.id);
+            }
+        });
+      }
+    });
+  },
+  
+  deleteFavoriteByID: function(favoriteID) {
+    var favorite = new ColorsForUs.Models.Favorite({ id: favoriteID });
+    
+    favorite.destroy({
+      success: function() {
+        console.log("destroyed!");
+      }
+    });
+  },
+  
   render: function() {
     var data = JSON.parse($("#bootstrapped_current_user_id").html());
     this.currentUserID = data.current_user_id;
