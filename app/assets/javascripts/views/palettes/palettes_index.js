3aaf6ac4182810ed5111697250eb3ee39d03e874
RussHR <russ.rinzler@gmail.com>
2013-11-08 10:37:28 -0800

    Favorite/unfavorite buttons swap upon click

--- app/assets/javascripts/views/palettes/palettes_index.js
+++ app/assets/javascripts/views/palettes/palettes_index.js
@@ -1,6 +1,16 @@
 ColorsForUs.Views.PalettesIndex = Backbone.View.extend({
   template: JST['palettes/index'],
   
+  events: {
+    "click button": "toggleFavorite"
+  },
+  
+  toggleFavorite: function(event) {
+    var $button = $(event.currentTarget);
+    var $buttons = $button.parent().children();
+    $buttons.toggleClass('favorite-button unfavorite-button');
+  },
+  
   render: function() {
     var data = JSON.parse($("#bootstrapped_current_user_id").html());
     var renderedContent = this.template({
